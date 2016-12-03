66e508d9c223d7b0d8fb1bcc3f5bbe3aa27a00ba
Orestis Markou <orestis@orestis.gr>
2016-12-03 17:13:17 +0100

    refactor vertical_triangles

--- 2016/day3.exs
+++ 2016/day3.exs
@@ -12,21 +12,21 @@ defmodule Day3 do
     |> IO.puts
   end
 
-  def vertical_triangles([l1, l2, l3]) do
-    List.zip([l1, l2, l3])
+  def vertical_triangles(l) do
+    Enum.chunk(l, 3)
+    |> Enum.map(&List.zip/1) #transpose
+    |> List.flatten()
+    |> Enum.map(&Tuple.to_list/1)
   end
 
   def solve2 do
     File.stream!("day3.txt")
     |> Enum.map(&String.trim/1)
     |> Enum.map(fn(line) -> String.split(line) |> Enum.map(&String.to_integer/1) end)
-    |> Enum.chunk(3)
-    |> Enum.map(&vertical_triangles/1)
-    |> List.flatten()
-    |> Enum.map(&Tuple.to_list/1)
+    |> vertical_triangles()
     |> Enum.map(&possible?/1)
     |> Enum.count(&(&1))
-    |> IO.puts
+    |> IO.inspect
   end
 end
 
@@ -42,7 +42,11 @@ defmodule Day3Test do
   end
 
   test "threes" do
-    assert [{101, 102, 103}, {301, 302, 303}, {501, 502, 503}] == vertical_triangles([
+    assert [[101, 102, 103], [301, 302, 303], [501, 502, 503]] == vertical_triangles([
       [101, 301, 501], [102, 302, 502], [103, 303, 503]])
+  end
+
+  test "part2" do
+    assert 1649 == solve2()
   end
 end
