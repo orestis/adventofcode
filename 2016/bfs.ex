0bf2c6588b8c64f50ca43625d123c5048852bd91
Orestis Markou <orestis@orestis.gr>
2016-12-22 13:42:58 +0200

    day 22

---
+++ 2016/bfs.ex
@@ -0,0 +1,34 @@
+defmodule BFS do
+  def walk_bfs(start, get_next, end_check) do
+    seen = MapSet.new()
+    steps = 0
+    walk_bfs([start], [], get_next, end_check, seen, steps)
+  end
+
+  def walk_bfs([], [], _get_next, _end_check, _seen, steps) do
+    IO.puts "giving up at steps #{steps}"
+    -1
+  end
+  def walk_bfs([], nq, get_next, end_check, seen, steps) do
+    IO.puts ">>exhausted depth #{steps}, going deeper, next check: #{length(nq)} nodes<<"
+    IO.puts "have seen so far #{MapSet.size(seen)}"
+      walk_bfs(nq, [], get_next, end_check, seen, steps+1)
+  end
+  def walk_bfs([curr|rest], nq, get_next, end_check, seen, steps) do
+    if end_check.(curr) do
+      IO.puts "FOUND #{inspect(curr)} at steps #{steps}"
+      steps
+    else
+      if not MapSet.member?(seen, curr) do
+        seen = MapSet.put(seen, curr)
+        children = get_next.(curr)
+        walk_bfs(rest, children ++ nq, get_next, end_check, seen, steps)
+      else
+        walk_bfs(rest, nq, get_next, end_check, seen, steps)
+      end
+        #|> Enum.reject(&(MapSet.member?(seen, &1)))
+ #     walk_bfs(rest, children ++ nq, get_next, end_check, seen, steps)
+    end
+  end
+
+end
--- 2016/day22.exs
+++ 2016/day22.exs
@@ -5,7 +5,11 @@ defmodule Day22 do
 
   defmodule Node do
     defstruct x: 0, y: 0, used: 0, avail: 0, total: 0
+    defimpl String.Chars, for: Node do
+      def to_string(node), do: inspect(node)
+    end
   end
+
 
   def parse("/dev/grid/node-" <> node) do
     r = ~r/x(\d+)-y(\d+)\s+(\d+)T\s+(\d+)T\s+(\d+)T.*/
@@ -14,8 +18,8 @@ defmodule Day22 do
     struct(Node, m)
   end
 
-  def input() do
-    File.stream!("day22.txt")
+  def input(fname \\ "day22.txt") do
+    File.stream!(fname)
     |> Stream.map(&String.trim/1)
     |> Stream.drop(2)
   end
@@ -51,6 +55,125 @@ defmodule Day22 do
     |> length()
   end
 
+  def make_grid(nodes, w, h) do
+    g = :digraph.new()
+
+    Enum.each(nodes, fn(node) ->
+      v = {node.x, node.y}
+      :digraph.add_vertex(g, v, node)
+    end)
+
+    g
+  end
+
+  def analyze_nodes(nodes) do
+    empty_node = Enum.sort_by(nodes, fn(n) -> n.avail end, &>=/2) |> Enum.take(1) |> Enum.at(0)
+    IO.puts "empty node is #{empty_node}"
+    empty_node
+  end
+
+  def print_grid(grid, w, h) do
+    IO.puts ""
+    IO.puts "~~~~~~~grid of #{w}x#{h}~~~~~~~~~"
+    for y <- 0..(h-1), x <- 0..(w-1) do
+
+      {{x, y}, node} = :digraph.vertex(grid, {x, y})
+      if x == 0 and y > 0 do
+        IO.puts ""
+      end
+      IO.write String.pad_leading("#{node.used}/", 3)
+      IO.write String.pad_leading("#{node.total}", 2)
+      if x < w-1, do: IO.write " - "
+    end
+    IO.puts ""
+
+  end
+
+  def find_path(initial, target, grid) do
+    require BFS
+    end_check = fn(node) -> node == target end
+    get_next = fn({x, y}) ->
+      {{x, y}, curr} = :digraph.vertex(grid, {x, y})
+      next_pos = [{x-1, y}, {x+1, y}, {x, y+1}, {x, y-1}]
+      next_nodes =
+        Enum.map(next_pos, fn(pos) -> :digraph.vertex(grid, pos) end)
+        |> Enum.reject(fn(node_or_false) -> node_or_false == false end)
+        |> Enum.map(fn({{x, y}, node}) -> node end)
+        |> Enum.filter(fn(node) -> node.used <= curr.total end)
+        |> Enum.map(fn(node) -> {node.x, node.y} end)
+
+      #IO.puts "node #{curr} has viable neighbors #{inspect(next_nodes)})"
+      next_nodes
+    end
+
+    steps = BFS.walk_bfs(initial, get_next, end_check)
+    IO.puts "can reach #{inspect(target)} from #{inspect(initial)} in #{steps} steps"
+
+    steps
+
+  end
+
+  def shortest_path(nodes) do
+    # assuming the instructions are not misleading and indeed
+    # the nodes are interchangeable, except of the empty one
+    # and the larges ones
+
+    # we can make a graph of all the nodes that are connected
+    # wherever there is an immovable node, we will leave it out
+    # and therefore sever the connections in the grid
+
+    # the first part is to bring the empty node to a spot adjacent
+    # to the Goal node
+    # there are four (or less) adjacent spots
+    # for each spot S, calculate the path between E -> S
+
+    # this is the first step of the problem, which is a simple
+    # "shortest_path" implementation
+    # as we use only 1 operation to move the empty node closer
+
+    # once we are adjacent to the Goal node, the grid is transformed
+    # to a weighted one; the weight or cost of each edge is the number
+    # of operations it takes to move the Goal node there
+    # this is at least 6 for an unobstructed grid but it could be more
+    # if there are unmovable nodes in the way
+
+    # once the costs are calculated, we can use Dijkstra's algorithm
+    # to find the shortest path from the Goal to Target (0, 0).
+    # To get the puzzle answer we get the total Dijkstra cost of the
+    # shortest path to the the cost of the first part of the problem
+
+  end
+
+  @tag :skip
+  test "part 2 test" do
+    nodes = get_nodes(input("day22.test.txt"))
+    empty_node = analyze_nodes(nodes)
+    grid = make_grid(nodes, 3, 3)
+    print_grid(grid, 3, 3)
+
+    find_path({empty_node.x, empty_node.y}, {2, 0}, grid)
+  end
+
+  test "part 2" do
+    nodes = get_nodes(input())
+    empty_node = analyze_nodes(nodes)
+    grid = make_grid(nodes, 38, 26)
+    print_grid(grid, 38, 3)
+    # 37 is the TARGET
+    # 36 is LEFT of TARGET
+    first_part = find_path({empty_node.x, empty_node.y}, {36, 0}, grid)
+    # we know how many moves to get to the LEFT of TARGET
+
+    # to get the empty node back to the ORIGIN we will need...
+    second_part = find_path({36, 0}, {0, 0}, grid)
+
+    # big assumption (to be tested)
+
+    IO.puts "we will need #{first_part} + 5 * #{second_part} + 1"
+    IO.puts "that is #{first_part + 5 * second_part + 1}"
+  end
+
+  @tag :skip
   test "all viable pairs" do
     nodes = get_nodes(input())
     assert all_viable_pairs(nodes) == nil
