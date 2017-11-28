2270332c8c09dabd14e5f98f49a372fcf0709307
Orestis Markou <orestis@orestis.gr>
2017-11-28 14:27:00 +0100

    day12 part 2 (and revised part1)

--- clojure/aoc/project.clj
+++ clojure/aoc/project.clj
@@ -4,7 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/math.combinatorics "0.1.4"]]
+                 [org.clojure/math.combinatorics "0.1.4"]
+                 [org.clojure/data.json "0.2.6"]]
   :main ^:skip-aot aoc.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- clojure/aoc/src/aoc/year2015_day12.clj
+++ clojure/aoc/src/aoc/year2015_day12.clj
@@ -1,11 +1,14 @@
 (ns aoc.year2015-day12
   (:use clojure.test)
   (:require [clojure.java.io :as io])
-  (:require [clojure.string :as str]))
+  (:require [clojure.walk :as walk])
+  (:require [clojure.string :as str])
+  (:require [clojure.data.json :as json]))
 
 
 (defonce input
-  (-> "2015/day12.txt" io/resource slurp))
+  (-> "2015/day12.txt" io/resource slurp json/read-str))
+
 
 ;; --- Day 12: JSAbacusFramework.io ---
 
@@ -28,9 +31,59 @@
 
 ;; What is the sum of all numbers in the document?
 
-(->> input
+(with-test
+  (defn numbers [d]
+    (let [result (atom [])]
+      (walk/prewalk (fn [x]
+                      (if (number? x)
+                        (do (swap! result conj x) nil)
+                        x)) d)
+      @result))
+  (testing "Numbers"
+    (doseq [[i n]
+            [
+             [[1,2,3], [1,2,3]]
+             [[1,{"c" "red", "b" 2},3], [1,2,3]]
+             [[[[3]]], [3]]
+             [{"a" [-1,1]}, [-1,1]]
+             ]]
+      (testing (str i)
+        (is (= n (numbers i)))))))
+
+#_(->> input
      (re-seq #"-?\d+")
      (map #(Integer/parseInt %))
      (reduce +))
 
+(reduce + (numbers input))
+
 ;; Your puzzle answer was 191164.
+
+;; --- Part Two ---
+
+;; Uh oh - the Accounting-Elves have realized that they double-counted everything
+;; red.
+
+;; Ignore any object (and all of its children) which has any property with the
+;; value "red". Do this only for objects ({...}), not arrays ([...]).
+
+(defn red-skipper [x]
+  (if (and (map? x) (some #{"red"} (vals x) ))
+    nil
+    x))
+
+(walk/prewalk red-skipper [1,2,3])
+(walk/prewalk red-skipper [1,{"c" "red" "b" 2},3])
+
+(reduce + (numbers (walk/prewalk red-skipper input)))
+
+
+
+;; [1,2,3] still has a sum of 6.
+;; [1,{"c":"red","b":2},3] now has a sum of 4, because the middle object is ignored.
+;; {"d":"red","e":[1,2,3,4],"f":5} now has a sum of 0, because the entire structure is ignored.
+;; [1,"red",5] has a sum of 6, because "red" in an array has no effect.
+
+;; Your puzzle answer was 87842.
+
+(run-all-tests #"aoc\.year2015-day12")
