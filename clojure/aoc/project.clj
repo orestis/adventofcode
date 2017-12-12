6339c32f44590518886a2199045ccb91bf220fbc
Orestis Markou <orestis@orestis.gr>
2017-12-12 09:40:05 +0100

    day12 parts 1+2

--- clojure/aoc/project.clj
+++ clojure/aoc/project.clj
@@ -5,6 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/math.combinatorics "0.1.4"]
+                 [ubergraph "0.4.0"]
                  [org.clojure/data.json "0.2.6"]]
   :main ^:skip-aot aoc.core
   :target-path "target/%s"
---
+++ clojure/aoc/src/aoc/2017_day12.clj
@@ -0,0 +1,105 @@
+(ns aoc.2017-day12
+  (:use clojure.test)
+  (:require [ubergraph.core :as uber])
+  (:require [ubergraph.alg :as uber-alg])
+  (:require [clojure.java.io :as io])
+  (:require [clojure.string :as str]))
+
+(def input
+  (-> "2017/day12.txt" io/resource slurp str/split-lines))
+
+;; --- Day 12: Digital Plumber ---
+
+;; Walking along the memory banks of the stream, you find a small village that
+;; is experiencing a little confusion: some programs can't communicate with each
+;; other.
+
+;; Programs in this village communicate using a fixed system of pipes. Messages
+;; are passed between programs using these pipes, but most programs aren't
+;; connected to each other directly. Instead, programs pass messages between
+;; each other until the message reaches the intended recipient.
+
+;; For some reason, though, some of these messages aren't ever reaching their
+;; intended recipient, and the programs suspect that some pipes are missing.
+;; They would like you to investigate.
+
+;; You walk through the village and record the ID of each program and the IDs
+;; with which it can communicate directly (your puzzle input). Each program has
+;; one or more programs with which it can communicate, and these pipes are
+;; bidirectional; if 8 says it can communicate with 11, then 11 will say it can
+;; communicate with 8.
+
+;; You need to figure out how many programs are in the group that contains
+;; program ID 0.
+
+;; For example, suppose you go door-to-door like a travelling salesman and
+;; record the following list:
+
+(def sample-input
+  ["0 <-> 2"
+   "1 <-> 1"
+   "2 <-> 0, 3, 4"
+   "3 <-> 2, 4"
+   "4 <-> 2, 3, 6"
+   "5 <-> 6"
+   "6 <-> 4, 5"])
+
+(defn parse [line]
+  (let [[from _ & to]
+        (clojure.edn/read-string (str "[" line "]"))]
+    [from to]))
+
+(defn make-graph [input]
+  (let [adjacency-map (into {} (map parse input))]
+    (uber/graph adjacency-map)))
+
+(def sample-graph
+  (make-graph sample-input))
+
+(uber-alg/scc sample-graph)
+;; => [[1] [5 6 4 3 2 0]]
+
+;; In this example, the following programs are in the group that contains
+;; program ID 0:
+
+;;     Program 0 by definition.
+;;     Program 2, directly connected to program 0.
+;;     Program 3 via program 2.
+;;     Program 4 via program 2.
+;;     Program 5 via programs 6, then 4, then 2.
+;;     Program 6 via programs 4, then 2.
+
+;; Therefore, a total of 6 programs are in this group; all but program 1, which
+;; has a pipe that connects it to itself.
+
+;; How many programs are in the group that contains program ID 0?
+
+(->> (make-graph input)
+     (uber-alg/scc)
+     (filter #(some #{0} %))
+     (first)
+     (count))
+;; => 306
+
+;; --- Part Two ---
+
+;; There are more programs than just the ones in the group containing program ID
+;; 0. The rest of them have no way of reaching that group, and still might have
+;; no way of reaching each other.
+
+;; A group is a collection of programs that can all communicate via pipes either
+;; directly or indirectly. The programs you identified just a moment ago are all
+;; part of the same group. Now, they would like you to determine the total
+;; number of groups.
+
+;; In the example above, there were 2 groups: one consisting of programs
+;; 0,2,3,4,5,6, and the other consisting solely of program 1.
+
+;; How many groups are there in total?
+
+(->> (make-graph input)
+     (uber-alg/scc)
+     (count))
+;; => 200
+
+
